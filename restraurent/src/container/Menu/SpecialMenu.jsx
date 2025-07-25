bba57bcdaa620fbd615bf1547bffc3c7ff29cc13
arindam-7777 <arindam81165@gmail.com>
2025-07-25 21:57:57 +0530

    Done with Award and Gallery.

--- restraurent/src/container/Menu/SpecialMenu.jsx
+++ restraurent/src/container/Menu/SpecialMenu.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { SubHeading } from '../../components'
+import { images, data } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
