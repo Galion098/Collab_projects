ef219144d68abd3d265cc4e8f9b7c86eec390765
Debajyoti jana <debajyotijana2004@gmail.com>
2025-07-27 15:41:41 +0530

    update intro

--- restraurent/src/container/Intro/Intro.jsx
+++ restraurent/src/container/Intro/Intro.jsx
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
-import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
-import{meal} from '../../constants';
+import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.css';
 
 const Intro = () => {
-  const[playVideo, setPlayvido] = React.useState();
-const vidRef = React.useRef();
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
 
-const handleVideo = () => {
-  setPlayvido((prevPlayVideo) => !prevPlayVideo);
-
-  if (playVideo) {vidRef.current.pause();
-  } else {
-    vidRef.current.play();
-}
-}
+  const handleVideo = () => {
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+  };
 
   return (
-  <div className="app__video">
-    <video
-    src={meal}  
-    ref={vidRef}
-    type="video/mp4"
-    loop
-    controls={false}
-    muted 
-    />
-    <div className="app__video-overlay flex __center">
-       <div 
-        className="app_video-overlay_circle flex_center"
-        onClick={handleVideo}
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circlr flex__center"
+          onClick={handleVideo}
         >
-        {playVideo
-        ? <BsPauseFill color="#fff" fontSize={30} />
-        : <BsFillPlayFill color="#fff" fontSize={30} />
-        }
-       </div>
+          {playVideo
+            ? <BsPauseFill color="#fff" fontSize={30} />
+            : <BsFillPlayFill color="#fff" fontSize={30} />
+          }
+        </div>
+      </div>
     </div>
-  </div>
-)
-}
+  );
+};
 
 export default Intro;
